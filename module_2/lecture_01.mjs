0e815931ff89e52ff85560de451d76b357c342ad
Ninas92 <ninas13@uia.no>
2024-09-04 14:54:33 +0200

    Finished task 2.1

--- module_2/lecture_01.mjs
+++ module_2/lecture_01.mjs
@@ -1,3 +1,6 @@
 import { initPrintOut, printOut, newLine } from "../../common/script/utils.mjs";
 initPrintOut(document.getElementById("txtOut"));
-printOut("Test");
+printOut("Hei på deg");
+
+const part1 = 2 + 3 * 2 - 4 * 6;
+printOut( "part1 = " + part1);
